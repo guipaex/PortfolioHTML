16558a53981b6e26ddb809c16e10c75598874d65
Guilherme Paes <paes.guilherme@hotmail.com>
2023-04-01 17:58:07 -0300

    creating language tags dinamicly

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-const user = "guipaex"; // SET Repositorie owner
-const url = `https://api.github.com/users/${user}/repos`; //Endpoint dos repositorios
+const user = "guipaex";
+const url = `https://api.github.com/users/${user}/repos`;
 
 const reposContainer = document.querySelector("[data-repos]")
 
@@ -10,38 +10,53 @@ axios.get(url).then( async response => {
 
 			const repoLink = repositories[i].html_url;							//Pega a URL do Repositório no GitHub
 			const projectName = repositories[i].name.replace(/[- ]+/g, " ")		//Pega o nome do Repositório e troca os "-" por espaço
-			const projectDescription = repositories[i].description;						//Pega Descrição do projeto
+			const projectDescription = repositories[i].description;				//Pega Descrição do projeto
 			const projectBuild = repositories[i].homepage;						//Pega a página de Build do projeto	
 			const projectAPI = repositories[i].url								//Pega a URL de API do projeto específico
 
 			//EndPoint das linguages do repositorio: https://api.github.com/repos/${user}/"nome-do-projeto"/languages`
 
-			let projectLangs = await axios.get(`${projectAPI}/languages`).then(response => {
+			const projectLangs = await axios.get(`${projectAPI}/languages`)
+			.then(response => {
 				const langs = response.data;
 				return langs
+			}).then( obj => {
+				return Object.keys(obj) //retorna um Array com as keys do objeto 'langs'(No caso, as keys são as linguagens)
 			}).catch(error => console.log(error))
 
-			console.log(`${projectAPI}/languages`)
-			console.log(projectLangs)
+			
+
+			// console.log(`${projectAPI}/languages`)
 			
 			//Exclui o repositório do READ.ME e os que não possuem LivePreview
 			if(	projectName !== user && projectBuild !== null){
-				createCard(projectName, projectDescription, repoLink, projectBuild)
+
+				const lang1 = projectLangs[0]
+				const lang2 = projectLangs[1]
+				const lang3 = projectLangs[2]
+
+				createCard(projectName, projectDescription, lang1, lang2, lang3, repoLink, projectBuild)
 			}
 		}
 	}).catch(error => console.log(error))
 
 
-	function createCard(title, description, gitLink, previewLink){
+	function createCard(title, description, lang1, lang2, lang3 , gitLink, previewLink){
 		
-		let card =
+		if (lang3 !== undefined){
+			lang3 = `<span class="repo__tag ${lang3}">${lang3}</span>`
+		} else {
+			lang3 = ""
+		}
+
+		const card =
 		`<div class="repo__card">
 			<h1 class="repo__title">${title}</h1>
 			<p class="repo__description">${description}</p>
-			<div class="repo__langs">
-				<span class="repo__tag HTML">HTML</span>
-				<span class="repo__tag CSS">CSS</span>
-				<span class="repo__tag JavaScript">JavaScript</span>
+			<div class="repo__langs" data-lang>
+				<span class="repo__tag ${lang1}">${lang1}</span>
+				<span class="repo__tag ${lang2}">${lang2}</span>
+				${lang3}
 			</div>
 			<div class="repo__links">
 				<a class="repo__github" href="${gitLink}"><img src="img/icons/github.svg"></a>
@@ -50,6 +65,7 @@ axios.get(url).then( async response => {
 		</div>`
 
 		reposContainer.innerHTML += card
+
 	}
 
 
