4165d817ea3a74aa0e2492506a7cc2a5e6527c5e
Guilherme Paes <paes.guilherme@hotmail.com>
2023-05-21 15:39:32 -0300

    mudança no método de filtragem

--- scripts/repositories.js
+++ scripts/repositories.js
@@ -6,40 +6,55 @@ const endpoint = `https://api.github.com/users/${user}/repos`;
 
 let projects = []
 
-async function getRepositories(){
-	try{
-		const result = await fetch(endpoint);
+async function getRepositories() {
+    try {
+        const result = await fetch(endpoint);
         const repos = await result.json();
 
-        repos.forEach( repository => {
-            let tagged = false
-            repository.topics.forEach(topic => {
-                if (topic = tagFiltro){
-                    tagged = true
-                };
-            });
-            if(tagged){
+        repos.forEach(async repository => {
+            let tagged = false;
+            repository.topics.forEach(async topic => {
+                if (topic = tagFiltro) {
+                    tagged = true;
+                }
+            })
+            if (tagged) {
                 const project = new Object();
-                project.name = repository.name
-                project.description = repository.description
-                project.repo = repository.html_url
-                project.page = repository.homepage
-                project.languages = getLanguages(repository.url)
+                project.name = repository.name;
+                project.description = repository.description;
+                project.repo = repository.html_url;
+                project.page = repository.homepage;
+                project.languages = getLanguages(repository.url);
                 projects.push(project)
             }
-        });
-	}
-	catch (erro){ 
+        })
+    } catch (erro) {
         console.log(erro)
     }
 }
-async function getLanguages(url){
-	try{
-		const langs = await fetch(`${url}/languages`);
-		return langs.json()
-	}
-	catch (erro) { console.log(erro)}
+
+async function getLanguages(url) {
+    try {
+        const langsURL = await fetch(`${url}/languages`);
+        const langs = await langsURL.json();
+        return langs;
+    }
+    catch (erro) {
+        console.log(erro)
+    }
 }
 
-getRepositories()
+async function getProjects() {
+    await getRepositories()
+    return projects
+}
+getProjects()
+    .then((projects) => {
+        projects.forEach(project => {
+            console.log(project)
+        })
+    })
+/*
 
+
+*/
